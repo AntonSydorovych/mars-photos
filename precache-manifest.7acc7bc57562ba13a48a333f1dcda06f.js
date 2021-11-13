8928758bab80f6d2c4e701fc9f889715f52539a7
Anton Sydorovych <antsyd2@gmail.com>
2021-11-13 03:18:19 +0200

    Updates

---
+++ precache-manifest.7acc7bc57562ba13a48a333f1dcda06f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7dcee2dd0c4cec8a930f56e9941af2",
+    "url": "/mars-photos/index.html"
+  },
+  {
+    "revision": "2f2b71e33bffec05f25d",
+    "url": "/mars-photos/static/css/2.f7861fd5.chunk.css"
+  },
+  {
+    "revision": "6e875ef6524a3e6720fe",
+    "url": "/mars-photos/static/css/main.4bcd4cb8.chunk.css"
+  },
+  {
+    "revision": "2f2b71e33bffec05f25d",
+    "url": "/mars-photos/static/js/2.a146255d.chunk.js"
+  },
+  {
+    "revision": "b78d06d995050d23ec508cb4ba84482c",
+    "url": "/mars-photos/static/js/2.a146255d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e875ef6524a3e6720fe",
+    "url": "/mars-photos/static/js/main.4ee1628e.chunk.js"
+  },
+  {
+    "revision": "49afd81692f5fd1fa7ca",
+    "url": "/mars-photos/static/js/runtime-main.c91ac1c1.js"
+  }
+]);
